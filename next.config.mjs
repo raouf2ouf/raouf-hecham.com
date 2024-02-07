ba49371e385bf99dfef36c0019df6610f7d2454e
Abdelraouf Hecham <raouf.hec@gmail.com>
2024-02-07 13:16:01 +0100

    fix: updated next config to use raouf-hecham as basepath

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,13 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false
 let assetPrefix = ''
 let basePath = '/'
 
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+// if (isGithubActions) {
+//   // trim off `<owner>/`
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
+//   assetPrefix = `/${repo}/`
+//   basePath = `/${repo}`
+// }
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
